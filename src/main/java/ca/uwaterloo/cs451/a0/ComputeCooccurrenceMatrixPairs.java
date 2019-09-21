81d8404a90f3f8a34daa73745f99f552fc9a3201
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-21 17:45:37 -0400

    Update ComputeCooccurrenceMatrixPairs.java

--- src/main/java/ca/uwaterloo/cs451/a0/ComputeCooccurrenceMatrixPairs.java
+++ src/main/java/ca/uwaterloo/cs451/a0/ComputeCooccurrenceMatrixPairs.java
@@ -167,10 +167,10 @@ public class ComputeCooccurrenceMatrixPairs extends Configured implements Tool {
       Configuration conf = context.getConfiguration();
       FileSystem fs = FileSystem.get(conf);
       
-      Path filePath = new Path(conf.get("intermediatePath").resolve("part-r-0000"));
+      Path yoPath = new Path(conf.get("intermediatePath"));
 //       Path filePath = new Path("/temp/part-r-0000");
 	    
-//       Path filePath = yoPath.resolve("part-r-00000");
+      Path filePath = yoPath.resolve("part-r-00000");
 
       if(!fs.exists(filePath)){
         throw new IOException("File Not Found: ");
