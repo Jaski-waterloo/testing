6c29368c0e558c7fce307aba611b3a9fac87eb3e
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-21 13:21:21 -0400

    Create findTotal.java

---
+++ src/main/java/ca/uwaterloo/cs451/a0/findTotal.java
@@ -0,0 +1,33 @@
+package ca.uwaterloo.cs451.a0;
+
+import java.io.File;
+import java.util.Scanner;
+import java.lang.Math;
+
+
+publiv static final class findTotal
+{
+	public String giveCount(String word) throws Exception
+	  {
+		  
+			  File file = new File("temp/part-r-00000");
+			  Scanner sc = new Scanner(file);
+		  while (sc.hasNextLine())
+                {
+                        String temp = sc.nextLine();
+                  
+                        String yo = "";
+                String[] arrOfStr = temp.split("\t");
+
+                if(arrOfStr[0].equals(word))
+			return(arrOfStr[1]);
+		  }
+		   return("No");
+	   }
+
+
+
+ public static void main(String[] args) throws Exception {
+  System.out.println(Int.ParseInt(giveCounts("a")));
+}
+}
