36a01f85fab7bc66c32d50915b4aacc7a37478e3
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-21 19:13:28 -0400

    Update ComputeCooccurrenceMatrixPairs.java

--- src/main/java/ca/uwaterloo/cs451/a0/ComputeCooccurrenceMatrixPairs.java
+++ src/main/java/ca/uwaterloo/cs451/a0/ComputeCooccurrenceMatrixPairs.java
@@ -140,8 +140,8 @@ public class ComputeCooccurrenceMatrixPairs extends Configured implements Tool {
         throws IOException, InterruptedException {
       List<String> tokens = Tokenizer.tokenize(value.toString());
 	    int size = tokens.size();
-// 	    if(size > 40)
-// 		    size = 40;
+	    if(size > 40)
+		    size = 40;
 
       for (int i = 0; i < size; i++) {
         for (int j = Math.max(i - window, 0); j < Math.min(i + window + 1, tokens.size()); j++) {
