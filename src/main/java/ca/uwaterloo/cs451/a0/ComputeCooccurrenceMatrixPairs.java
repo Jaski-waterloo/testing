4ce9ac221e05f87a1b8c016e2f8ac352461102fd
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-22 15:11:44 -0400

    Update ComputeCooccurrenceMatrixPairs.java

--- src/main/java/ca/uwaterloo/cs451/a0/ComputeCooccurrenceMatrixPairs.java
+++ src/main/java/ca/uwaterloo/cs451/a0/ComputeCooccurrenceMatrixPairs.java
@@ -224,9 +224,9 @@ public class ComputeCooccurrenceMatrixPairs extends Configured implements Tool {
         	    int X = total.get(x);
         	    int Y = total.get(y);
 
-        	    pmi = Math.log10((Both * totalSum) / (X * Y));
+//         	    pmi = Math.log10((Both * totalSum) / (X * Y));
 		    
-		    PMI.set(pmi);
+		    PMI.set(Math.log10((Both * totalSum) / (X * Y)));
 
       		   context.write(key, PMI);
     }
