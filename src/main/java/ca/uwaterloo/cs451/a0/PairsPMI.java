b5321f1ece2e80304988aa43438dc1ca1ca4d4f3
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-23 12:30:33 -0400

    Update PairsPMI.java

--- src/main/java/ca/uwaterloo/cs451/a0/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs451/a0/PairsPMI.java
@@ -4,9 +4,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 import tl.lin.data.pair.PairOfStrings;
 import tl.lin.data.pair.PairOfFloats;
 
-import java.util.Iterator;
-import java.util.List;
-
 import io.bespin.java.util.Tokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
