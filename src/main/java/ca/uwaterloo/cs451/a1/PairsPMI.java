9c983716698e4c5223ba63affefc9a5c15e6cbbd
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-24 11:58:36 -0400

    Update PairsPMI.java

--- src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
@@ -83,7 +83,7 @@ public class PairsPMI extends Configured implements Tool {
 	      i += 1;
       }
 	    for(String word : hash_Set){
-        WORD.set((Text)word);
+        WORD.set(word);
         context.write(WORD, ONE);
 	    }
       }
