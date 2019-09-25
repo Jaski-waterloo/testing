0301129bb2cdd0cb8b093e52a32918560b5baccd
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-25 14:28:00 -0400

    Update BigramCount.scala

--- src/main/scala/ca/uwaterloo/cs451/a2/BigramCount.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/BigramCount.scala
@@ -100,7 +100,7 @@ object BigramCount extends Configured with Tool with WritableConversions with To
       for (value <- values.asScala) {
         sum += value
       }
-     var strkey = key.asInstanceOf[String]
+     var strkey = (String)key.asScala
      if(strkey.takeRight(1) == "*"){
       val1.set((sum).asInstanceOf[Float])
       context.write(key, val1)
