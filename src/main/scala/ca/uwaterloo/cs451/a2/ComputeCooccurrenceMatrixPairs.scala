0a3f11bb5182721f7fefbf4661a52f9324e14971
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-26 13:47:11 -0400

    Update ComputeCooccurrenceMatrixPairs.scala

--- src/main/scala/ca/uwaterloo/cs451/a2/ComputeCooccurrenceMatrixPairs.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/ComputeCooccurrenceMatrixPairs.scala
@@ -59,7 +59,7 @@ object ComputeCooccurrenceMatrixPairs extends Configured with Tool with Writable
       val tokens1 = tokenize(value)
      var s = Set(tokens1)
      val tokens = s.toArray
-      for (i <- tokens.indices) {
+      for (i <- tokens.size) {
         for (j <- Math.max(i - window, 0) until Math.min(i + window + 1, tokens.length)) {
           if (i != j) context.write(new PairOfStrings(tokens(i), tokens(j)), 1)
         }
