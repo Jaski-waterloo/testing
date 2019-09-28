4dd74ec66f33bb49a8710b2eb72036ce6b6aa7a1
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-09-28 15:03:59 -0400

    Update WordCount.scala

--- src/main/scala/ca/uwaterloo/cs451/a2/WordCount.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/WordCount.scala
@@ -13,7 +13,7 @@ object WordCount {
       // Load our input data.
       val input =  sc.textFile(inputFile)
       // Split up into words.
-      val words = input.flatMap(line => line.split(" "))
+      val words = input.flatMap(line => line[:40].split(" "))
       // Transform into word and count.
       val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
       // Save the word count back out to a text file, causing evaluation.
