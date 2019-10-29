585314067cf045afe3072ab982e9ea61627595a3
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-10-29 19:48:34 -0400

    Create Q1.scala

---
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
@@ -0,0 +1,18 @@
+import io.bespin.scala.util.Tokenizer
+
+import collection.mutable.HashMap
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+
+class Conf(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, output, reducers)
+  val input = opt[String](descr = "input path", required = true)
+  val output = opt[String](descr = "output path", required = true)
+  val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
+  val imc = opt[Boolean](descr = "use in-mapper combining", required = false)
+  verify()
+}
