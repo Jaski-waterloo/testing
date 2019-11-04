9d77715727c41c4c34cf6d92156bd6d115cb6fa6
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-04 13:12:08 -0500

    Update Q6.scala

--- src/main/scala/ca/uwaterloo/cs451/a5/Q6.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q6.scala
@@ -23,7 +23,7 @@ class ConfQ4(args: Seq[String]) extends ScallopConf(args) {
   verify()
 }
 
-object Q4 extends Tokenizer 
+object Q6 extends Tokenizer 
 {
    val log = Logger.getLogger(getClass().getName())
   
@@ -32,7 +32,7 @@ object Q4 extends Tokenizer
   
   
    def main(argv: Array[String]) {
-    val args = new ConfQ4(argv)
+    val args = new ConfQ6(argv)
 
     log.info("Input: " + args.input())
     log.info("Date : " + args.date())
@@ -41,9 +41,49 @@ object Q4 extends Tokenizer
      log.info("Text Data : " + args.text())
      log.info("Parquet Data : " + args.parquet())
 
-    val conf = new SparkConf().setAppName("Q4")
+    val conf = new SparkConf().setAppName("Q6")
     val sc = new SparkContext(conf)
      
 //      val count = sc.accumulator(0, "accumulator");
 //      val date = sc.broadcast(args.date())
-val date = args.date()
+    val date = args.date()
+    
+    val lineitems = sc.textFile(args.input() + "/lineitems.tbl")
+    lineitems.
+    filter(line => {
+      line.split('|')(10) contains date
+    })
+    .map(line => {
+      a = line.split('|')
+      retFlag = a(8)
+      lineStatus = a(9)
+      l_quantity = a(4).toDouble
+      l_extendedprice = a(5).toDouble
+      l_discount = a(6).toDouble
+      l_tax = a(7).toDouble
+      
+      ((retFlag, lineStatus), (l_quantity, l_extendedprice, l_extendedprice*(1-l_discount), l_extendedprice*(1-l_discount)*(1+l_tax), l_discount, 1))
+      
+    })
+    .reduceByKey((a,b) => {
+      (a._1 + b._1, a._2 + b._2, a._3 + b._3, a._4 + b._4, a._5 + b._5, a._6 + a._6)
+    })
+    .collect()
+    .foreach(p => {
+      count = p._6
+//       l_returnflag,
+//       l_linestatus,
+      sum_qty = a._1
+      sum_base_price = a._2
+      sum_disc_price = a._3
+      sum_charge = a._4
+      avg_qty = a._1 / count
+      avg_price = a._2 / count
+      avg_disc = a._5 / count
+//       count(*) as count_order
+      println((sum_qty, sum_base_price, sum_disc_price, sum_charge, avg_qty, avg_price, avg_disc, count))
+    })
+   }
+}
+      
+      
