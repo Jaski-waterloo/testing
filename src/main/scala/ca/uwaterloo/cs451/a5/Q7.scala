bdf7b05e06a40ff6c10d296ee9348820a4899cf9
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-04 17:01:19 -0500

    Update Q7.scala

--- src/main/scala/ca/uwaterloo/cs451/a5/Q7.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q7.scala
@@ -1,1 +1,74 @@
+package ca.uwaterloo.cs451.a5
 
+import io.bespin.scala.util.Tokenizer
+
+import collection.mutable.HashMap
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import scala.util.Try
+
+class ConfQ7(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, data)
+  val input = opt[String](descr = "input path", required = true)
+  //   val output = opt[String](descr = "output path", required = true)
+//   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
+//   val imc = opt[Boolean](descr = "use in-mapper combining", required = false)
+  val date = opt[String](descr = "date of Select Query", required = true)
+  val text = opt[Boolean](descr = "Use Text Data", required = false)
+  val parquet = opt[Boolean](descr = "Use parquet Data", required = false)
+  verify()
+}
+
+
+object Q7 extends Tokenizer
+{
+  val logger = Logger.getLogger(getClass().getName())
+  
+  //   val outputDir = new Path(args.output())
+//   FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+  
+  def main(argv: Array[String]) {
+    val args = new ConfQ7(argv)
+    
+    log.info("Input: " + args.input())
+    log.info("Date: " + args.date())
+//     log.info("Output: " + args.output())
+//     log.info("Number of reducers: " + args.reducers())
+    log.info("Text Data? : " + args.text())
+    log.info("Parquet Data? : " + args.parquet())
+    
+    val conf = new SparkConf().setAppName("Q7")
+    val sc = new SparkContext(conf)
+    
+    val date = args.date().split('-')
+    
+    val customer = sc.textFile(args.input() + "/customer.tbl")
+    .map(line => {
+      val a = line.split('|')
+      (a(0).toInt, a(1))
+    })
+    
+    val bcustomer = sc.broadcast(customer.collectAsMap())
+    
+    val lineitem = sc.textFile(args.input() + "/lineitem.tbl")
+    .map(line => {
+      val a = line.split('|')
+      (a(0).toInt, a(10))
+    })
+    .filter(p => {
+      date1 = p._2.split('-') // year-month-day // lshipdate > date
+      if(date1(0) > date(0)){
+        if(date1(1) > date(1){
+          if(date1(2) > date(2)) true
+        }
+           }
+      else false
+    })
+    
+      }
+    }
+            
