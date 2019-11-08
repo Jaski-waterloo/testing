8a8e75de1a8e8b5ecb20de67b4cf2f656f40568d
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-08 16:34:10 -0500

    Update Q1.scala

--- src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
@@ -55,27 +55,27 @@ object Q1 extends Tokenizer {
      {
         val sparkSession = SparkSession.builder.getOrCreate
       val lineitemDF = sparkSession.read.parquet(args.input() + "/lineitem")
-//       textFile = lineitemDF.rdd
+      textFile = lineitemDF.rdd
      }
      
-//      val count = sc.accumulator(0, "accumulator");
-// //      val date = sc.broadcast(args.date())
-//      val date = args.date();
+     val count = sc.accumulator(0, "accumulator");
+//      val date = sc.broadcast(args.date())
+     val date = args.date();
 
-//      textFile.map(line=> {
-//        val tokens = line.split('|')
-// //        var arrayname = new Array[datatype](tokens.length)
-//        tokens
-//      })
-//      .map(line => line(10))
-//      .foreach(line =>
-//               if(line contains date)
-//               {
-//                 count += 1;
-//               })
-// //      .saveAsTextFile("testoutput")
+     textFile.map(line=> {
+       val tokens = line.split('|')
+//        var arrayname = new Array[datatype](tokens.length)
+       tokens
+     })
+     .map(line => line(10))
+     .foreach(line =>
+              if(line contains date)
+              {
+                count += 1;
+              })
+//      .saveAsTextFile("testoutput")
      
-//      println("ANSWER=" + count.value);
+     println("ANSWER=" + count.value);
               
        
 
