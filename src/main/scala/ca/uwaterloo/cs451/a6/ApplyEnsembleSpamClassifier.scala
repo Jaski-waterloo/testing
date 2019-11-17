dad93295422c2bba6485509aa975756a3325ae24
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-17 15:15:26 -0500

    Update ApplyEnsembleSpamClassifier.scala

--- src/main/scala/ca/uwaterloo/cs451/a6/ApplyEnsembleSpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs451/a6/ApplyEnsembleSpamClassifier.scala
@@ -1,62 +1,62 @@
-package ca.uwaterloo.cs451.a6
+// package ca.uwaterloo.cs451.a6
 
-import io.bespin.scala.util.Tokenizer
+// import io.bespin.scala.util.Tokenizer
 
-import org.apache.log4j._
-import org.apache.hadoop.fs._
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
-import org.rogach.scallop._
+// import org.apache.log4j._
+// import org.apache.hadoop.fs._
+// import org.apache.spark.SparkContext
+// import org.apache.spark.SparkConf
+// import org.rogach.scallop._
 
-class Conf3(args: Seq[String]) extends ScallopConf(args) {
-  mainOptions = Seq(input, output, model)
-  val input = opt[String](descr = "input path", required = true)
-  val output = opt[String](descr = "output path", required = true)
-  val method = opt[String](descr = "method", required = true)
-  verify()
-}
+// class Conf3(args: Seq[String]) extends ScallopConf(args) {
+//   mainOptions = Seq(input, output, model)
+//   val input = opt[String](descr = "input path", required = true)
+//   val output = opt[String](descr = "output path", required = true)
+//   val method = opt[String](descr = "method", required = true)
+//   verify()
+// }
 
-object ApplyEnsembleSpamClassifier {
-	val log = Logger.getLogger(getClass().getName())
+// object ApplyEnsembleSpamClassifier {
+// 	val log = Logger.getLogger(getClass().getName())
 
-	def main(argv: Array[String]) {
-		val args = new Conf3(argv)
+// 	def main(argv: Array[String]) {
+// 		val args = new Conf3(argv)
 
-		log.info("Input: " + args.input())
-		log.info("Output: " + args.output())
-		log.info("Model: " + args.model())
+// 		log.info("Input: " + args.input())
+// 		log.info("Output: " + args.output())
+// 		log.info("Model: " + args.model())
 
-		val conf = new SparkConf().setAppName("Apply Ensemble Spam Classifier")
-		val sc = new SparkContext(conf)
+// 		val conf = new SparkConf().setAppName("Apply Ensemble Spam Classifier")
+// 		val sc = new SparkContext(conf)
 
-		val outputDir = new Path(args.output())
-		FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+// 		val outputDir = new Path(args.output())
+// 		FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
-		val textFile = sc.textFile(args.input())
+// 		val textFile = sc.textFile(args.input())
 		
-		val model = sc.textFile(args.model() + "/part-00000")
-		.map(line => {
-			val tokens = line.substring(1, line.length()-1).split(',')
-			(tokens(0).toInt, tokens(1).toDouble)
-		})
+// 		val model = sc.textFile(args.model() + "/part-00000")
+// 		.map(line => {
+// 			val tokens = line.substring(1, line.length()-1).split(',')
+// 			(tokens(0).toInt, tokens(1).toDouble)
+// 		})
 
-		val bModel = sc.broadcast(model.collectAsMap())
+// 		val bModel = sc.broadcast(model.collectAsMap())
 
-		def spamminess(features: Array[Int]) : Double = {
-			var score = 0d
-			features.foreach(f => if (bModel.value.contains(f)) score += bModel.value(f))
-			score
-		}
+// 		def spamminess(features: Array[Int]) : Double = {
+// 			var score = 0d
+// 			features.foreach(f => if (bModel.value.contains(f)) score += bModel.value(f))
+// 			score
+// 		}
 
-		val tested = textFile.map(line => {
-			val tokens = line.split(" ")
-			val features = tokens.drop(2).map(_.toInt)
-			val score = spamminess(features)
-			var isSpam = "ham"
-			if (score > 0) isSpam = "spam"
-			(tokens(0), tokens(1), score, isSpam)
-		})
+// 		val tested = textFile.map(line => {
+// 			val tokens = line.split(" ")
+// 			val features = tokens.drop(2).map(_.toInt)
+// 			val score = spamminess(features)
+// 			var isSpam = "ham"
+// 			if (score > 0) isSpam = "spam"
+// 			(tokens(0), tokens(1), score, isSpam)
+// 		})
 
-		tested.saveAsTextFile(args.output())
-	}
-}
+// 		tested.saveAsTextFile(args.output())
+// 	}
+// }
