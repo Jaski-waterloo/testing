9c018aefc5c8deb00eac1a5c5e50c07415278082
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-17 14:36:50 -0500

    Update TrainSpamClassifier.scala

--- src/main/scala/ca/uwaterloo/cs451/a6/TrainSpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs451/a6/TrainSpamClassifier.scala
@@ -7,33 +7,68 @@ import org.apache.hadoop.fs._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
+import scala.math.exp
 
-class ConfPairs(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
-  mainOptions = Seq(input, output, reducers, threshold)
+class ConfTrain(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, model)
   val input = opt[String](descr = "input path", required = true)
-  val output = opt[String](descr = "output path", required = true)
-  val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
-  val threshold = opt[Int](descr = "threshold", required = false, default = Some(10))
-  val numExecutors = opt[Int](descr = "number of executors", required = false, default = Some(1))
-  val executorCores = opt[Int](descr = "number of cores", required = false, default = Some(1))
+  val model = opt[String](descr = "model path", required = true)
   verify()
 }
 
-object PairsPMI extends Tokenizer {
-  val log = Logger.getLogger(getClass().getName())
+object TrainSpamClassifier {
+	val log = Logger.getLogger(getClass().getName())
 
-  def main(argv: Array[String]) {
-    val args = new ConfPairs(argv)
+	def main(argv: Array[String]) {
+		val args = new ConfTrain(argv)
 
-    log.info("Input: " + args.input())
-    log.info("Output: " + args.output())
-    log.info("Number of reducers: " + args.reducers())
+		log.info("Input: " + args.input())
+		log.info("Model: " + args.model())
 
-    val conf = new SparkConf().setAppName("Pairs PMI")
-    val sc = new SparkContext(conf)
-    val threshold = args.threshold()
+		val conf = new SparkConf().setAppName("Train Spam Classifier")
+		val sc = new SparkContext(conf)
 
-    val outputDir = new Path(args.output())
-    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+		val outputDir = new Path(args.model())
+		FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
 
-val textFile = sc.textFile(args.input(), args.reducers())
+		var textFile = sc.textFile(args.input())
+
+		val w = scala.collection.mutable.Map[Int, Double]()
+
+		def spamminess(features: Array[Int]) : Double = {
+			var score = 0d
+			features.foreach(f => if (w.contains(f)) score += w(f))
+			score
+		}
+
+		val delta = 0.002
+
+		val trained = textFile.map(line =>{
+			val tokens = line.split(" ")
+			val docid = tokens(0)
+      var isSpam = 0d
+      if(tokens(1) == "spam") isSpam = 1d
+			val features = tokens.drop(2).map(_.toInt)
+			(0, (docid, isSpam, features))
+		})
+		.groupByKey(1)
+		.flatMap(p => {
+			p._2.foreach(item => {
+				val features = item._3
+				val isSpam = item._2
+				val score = spamminess(features)
+				val prob = 1.0 / (1 + exp(-score))
+				features.foreach(f => {
+					if (w.contains(f)) {
+						w(f) += (isSpam - prob) * delta
+					} else {
+						w(f) = (isSpam - prob) * delta
+					}
+				})
+			})
+			w
+		})
+
+		trained.saveAsTextFile(args.model())
+	}
+}
